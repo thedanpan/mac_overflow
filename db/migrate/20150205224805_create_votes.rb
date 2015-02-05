fae840bfa116cad07dfa9bbd9fae088ce1a0da02
Darrin Reeves & Pearl Shin & Ricky Thomas & Daniella Shin <pair+dxr4841.pearlshin.ricky-thomas.thedanpan@devbootcamp.com>
2015-02-05 17:54:36 -0500

    add votes migration

--- db/migrate/20150205224805_create_votes.rb
+++ db/migrate/20150205224805_create_votes.rb
@@ -1,6 +1,11 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
+      t.integer :votable_id
+      t.integer :user_id
+      t.string :votable_type
+
+      t.timestamps null: false
     end
   end
 end
