9b9a3303108ce0b195744bc99b7462a065fe9b75
Darrin Reeves & Pearl Shin & Ricky Thomas & Daniella Shin <pair+dxr4841.pearlshin.ricky-thomas.thedanpan@devbootcamp.com>
2015-02-05 17:52:17 -0500

    add questions migration

--- db/migrate/20150205224756_create_questions.rb
+++ db/migrate/20150205224756_create_questions.rb
@@ -1,6 +1,10 @@
 class CreateQuestions < ActiveRecord::Migration
   def change
     create_table :questions do |t|
+      t.text :prompt
+      t.integer :user_id
+
+      t.timestamps null: false
     end
   end
 end
