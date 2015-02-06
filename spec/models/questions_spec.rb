b8fdccac252ff54212e258b51a9416f1abb7a361
Ricky-Thomas <rickythomas.rt@gmail.com>
2015-02-05 22:03:23 -0500

    Tests whether a question object is a question object

--- spec/models/questions_spec.rb
+++ spec/models/questions_spec.rb
@@ -2,6 +2,11 @@ require 'rails_helper'
 
 describe Question do
 
+
+  it 'returns a Question object' do
+   expect(build(:question)).to be_a(Question)
+  end
+
   it "has a valid factory" do
     expect(build(:question)).to be_valid
   end
