a92bb482da9ad100630e77a205c13c434d870bed
Ricky-Thomas <rickythomas.rt@gmail.com>
2015-02-05 21:49:46 -0500

    Test for question title validation

--- spec/models/questions_spec.rb
+++ spec/models/questions_spec.rb
@@ -5,4 +5,8 @@ describe Question do
   it "has a valid factory" do
     expect(build(:question)).to be_valid
   end
+
+  it "is invalid without a title" do
+    expect(build(:question, title: nil).valid?).to eq false
+  end
 end
