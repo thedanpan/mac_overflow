2ff9a76e0bf09c09d0219226bbaff1d6dc75fb31
Daniella Shin <thedanpan@gmail.com>
2015-02-06 12:25:04 -0500

    refactor model vote_spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,18 +1,20 @@
 require 'rails_helper'
 
 describe Vote do
+  it "has a valid factory" do
+    expect(build(:vote)).to be_valid
+  end
+
   it 'returns a Vote object' do
     expect(build(:vote)).to be_a(Vote)
   end
-end
 
-describe Vote do
   it "has a votable_id that is a Fixnum" do
     expect(build(:vote)[:votable_id]).to be_an_instance_of(Fixnum)
   end
 
   it "has a votable_type that is a question" do
-    expect(build(:vote)[:votable_type]).to eq('question')
+    expect(build(:vote)[:votable_type]).to satisfy { |value| value == 'question' || value == 'answer'}
   end
 
   it "has a user_id that is a Fixnum" do
