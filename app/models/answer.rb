659a9ceade3ea5770092eec596d451648c8656ba
Daniella Shin <thedanpan@gmail.com>
2015-02-06 12:22:18 -0500

    add answer model validation of content, question_id, and user_id

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -3,4 +3,6 @@ class Answer < ActiveRecord::Base
   belongs_to :user
   has_many :comments, as: :commentable
   has_many :votes, as: :votable
+
+  validates_presence_of :content, :question_id, :user_id
 end
