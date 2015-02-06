4e6c61af0717d40efa9d7ea044bc26d257983270
Ricky Thomas <rickythomas.rt@gmail.com>
2015-02-06 14:37:46 -0500

    Adds questions new action

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,18 @@
 class QuestionsController < ApplicationController
 
+
+#GET /questions
   def index
     @questions = Question.all
+  end
+
+#GET /questions/new
+  def new
+    if current_user
+      render 'new'
+    else
+      redirect_to :root
+    end
   end
 
 end
