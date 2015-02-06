6f3aa6d1988ad48db028e62e9d819bd40571cc25
Daniella Shin <thedanpan@gmail.com>
2015-02-06 15:45:02 -0500

    add date posted and date updated formatting methods to answer model

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,4 +5,12 @@ class Answer < ActiveRecord::Base
   has_many :votes, as: :votable
 
   validates_presence_of :content, :question_id, :user_id
+
+  def created_at
+    created_at.strftime("%D %R")
+  end
+
+  def updated_at
+    updated_at.strftime("%D %R")
+  end
 end
