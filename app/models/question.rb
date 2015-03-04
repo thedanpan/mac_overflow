4704d8578061c020b76b998ae3470617e1df6522
Pearl Shin <pearlshin22@gmail.com>
2015-03-04 17:11:41 -0500

    add question and answer methods for edited at and created at

--- app/models/question.rb
+++ app/models/question.rb
@@ -5,4 +5,37 @@ class Question < ActiveRecord::Base
   has_many :votes, as: :votable
 
   validates_presence_of :title, :prompt, :user_id
+
+
+  def edited_time
+    if Time.now - self.updated_at < 60
+      (Time.now.utc - self.updated_at).round(0) + " seconds ago"
+    elsif Time.now - self.updated_at > 60 && Time.now - self.updated_at < 3600
+      ((Time.now.utc - self.updated_at).round(0)/60).to_s + " minutes ago"
+    # minutes
+    elsif Time.now.utc - self.updated_at > 3600 && Time.now - self.updated_at < 86400
+      ((Time.now.utc - self.updated_at).round(0)/60/60).to_s + " hours ago"
+    # hours
+    else # days
+      ((Time.now.utc - self.updated_at).round(0)/60/60/60).to_s + " days ago"
+    end
+  end
+
+
+  def answered_time
+    if Time.now - self.created_at < 60
+      (Time.now.utc - self.created_at).round(0) + " seconds ago"
+    elsif Time.now - self.created_at > 60 && Time.now - self.created_at < 3600
+      ((Time.now.utc - self.created_at).round(0)/60).to_s + " minutes ago"
+    # minutes
+    elsif Time.now.utc - self.created_at > 3600 && Time.now - self.created_at < 86400
+      ((Time.now.utc - self.created_at).round(0)/60/60).to_s + " hours ago"
+    # hours
+    else # days
+      ((Time.now.utc - self.created_at).round(0)/60/60/60).to_s + " days ago"
+    end
+  end
+
+
+
 end
