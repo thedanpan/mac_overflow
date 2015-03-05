7f54babaca8305bfe44ca0ff216a00e19c0ecff3
Pearl Shin <pearlshin22@gmail.com>
2015-03-04 22:21:55 -0500

    fix math to calcalate number of days since question or answer has been posted

--- app/models/question.rb
+++ app/models/question.rb
@@ -17,7 +17,7 @@ class Question < ActiveRecord::Base
       ((Time.now.utc - self.updated_at).round(0)/60/60).to_s + " hours ago"
     # hours
     else # days
-      ((Time.now.utc - self.updated_at).round(0)/60/60/60).to_s + " days ago"
+      ((Time.now.utc - self.updated_at).round(0)/60/60/24).to_s + " days ago"
     end
   end
 
@@ -32,7 +32,7 @@ class Question < ActiveRecord::Base
       ((Time.now.utc - self.created_at).round(0)/60/60).to_s + " hours ago"
     # hours
     else # days
-      ((Time.now.utc - self.created_at).round(0)/60/60/60).to_s + " days ago"
+      ((Time.now.utc - self.created_at).round(0)/60/60/24).to_s + " days ago"
     end
   end
 
