6673feef4d1ba1b1cf9641cbf890c417d0739466
Pearl Shin <pearlshin22@gmail.com>
2015-03-05 20:13:07 -0500

    take out edited time and answered methods and move to helper methods

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,34 +7,6 @@ class Question < ActiveRecord::Base
   validates_presence_of :title, :prompt, :user_id
 
 
-  def edited_time
-    if Time.now - self.updated_at < 60
-      (Time.now.utc - self.updated_at).round(0).to_s + " seconds ago"
-    elsif Time.now - self.updated_at > 60 && Time.now - self.updated_at < 3600
-      ((Time.now.utc - self.updated_at).round(0)/60).to_s + " minutes ago"
-    # minutes
-    elsif Time.now.utc - self.updated_at > 3600 && Time.now - self.updated_at < 86400
-      ((Time.now.utc - self.updated_at).round(0)/60/60).to_s + " hours ago"
-    # hours
-    else # days
-      ((Time.now.utc - self.updated_at).round(0)/60/60/24).to_s + " days ago"
-    end
-  end
-
-
-  def answered_time
-    if Time.now - self.created_at < 60
-      (Time.now.utc - self.created_at).round(0) + " seconds ago"
-    elsif Time.now - self.created_at > 60 && Time.now - self.created_at < 3600
-      ((Time.now.utc - self.created_at).round(0)/60).to_s + " minutes ago"
-    # minutes
-    elsif Time.now.utc - self.created_at > 3600 && Time.now - self.created_at < 86400
-      ((Time.now.utc - self.created_at).round(0)/60/60).to_s + " hours ago"
-    # hours
-    else # days
-      ((Time.now.utc - self.created_at).round(0)/60/60/24).to_s + " days ago"
-    end
-  end
 
 
 
